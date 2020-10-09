2f1d461b41326178076476fc642dde7b6fcd7eac
hsriver <hase12prog@gmail.com>
2020-10-09 18:29:50 +0900

    aaa

--- js/main.js
+++ js/main.js
@@ -1,22 +1,39 @@
-(function() {
-  'use strict';
+(function () {
+  "use strict";
 
-var vm = new Vue({
-  el: '#app',
-  data: {
-    newItem: '',
-    todos: [
-      'task 1',
-      'task 2',
-      'task 3',
-    ]
-  },
-  methods: {
-    addItem: function() {
-      this.todos.push(this.newItem);
-      this.newItem = '';
-    }
-  }
-});
-
+  var vm = new Vue({
+    el: "#app",
+    data: {
+      newItem: "",
+      todos: [
+        {
+          title: "task 1",
+          isDone: false,
+        },
+        {
+          title: "task 2",
+          isDone: false,
+        },
+        {
+          title: "task 3",
+          isDone: true,
+        },
+      ],
+    },
+    methods: {
+      addItem: function () {
+        var item = {
+          title: this.newItem,
+          isDone: false
+        };
+        this.todos.push(item);
+        this.newItem = "";
+      },
+      deleteItem: function (index) {
+        if (confirm("are you sure?")) {
+          this.todos.splice(index, 1);
+        }
+      },
+    },
+  });
 })();
