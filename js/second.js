42940e942a5cf803cdefc55468206e37d1235ff0
hsriver <hase12prog@gmail.com>
2020-10-13 23:43:02 +0900

    vue is awesome!

--- js/second.js
+++ js/second.js
@@ -2,15 +2,23 @@
   "use strict";
 
   var likeComponent = Vue.extend({
+    // props: ['message'],
+    props: {
+      message: {
+        type: String,
+        default: 'Like'
+      }
+    },
     data: function() {
       return {
         count: 0
       }
     },
-    template: '<button @click="countUp">Like{{ count }}</button>',
+    template: '<button @click="countUp">{{ message }}{{ count }}</button>',
     methods: {
       countUp: function() {
         this.count++;
+        this.$emit('increment');
       }
     }
   });
@@ -19,6 +27,14 @@
     el: "#app",
     components: {
       'like-component': likeComponent
+    },
+    data: {
+      total: 0
+    },
+    methods: {
+      incrementTotal: function() {
+        this.total++;
+      }
     }
   });
   
